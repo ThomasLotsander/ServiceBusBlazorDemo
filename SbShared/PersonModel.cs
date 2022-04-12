0c257777097e2467d26a571f21a71b8861f788bc
Thomas Lotsander <thomas.lotsander@rikstv.no>
2022-04-12 16:13:54 +0200

    Add person model

---
+++ SbShared/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SbShared
+{
+    public class PersonModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
